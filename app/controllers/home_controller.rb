d418b355065ac49a12f391662f87cacb192936c2
zadi0648 <zackd44@gmail.com>
2013-12-18 19:09:15 -0800

    Made various changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ApplicationController
             redirect_to new_user_session_path
         else
         @post = Post.new
-        followees_ids = current_user.followees(User).pluck(:id)
-        #get only the ids of the people current_user folllows
-        #followees_ids << current_user.id
-        @activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User")
+        friends_ids = current_user.followees(User).pluck(:id)
+	friends_ids << current_user.id # Allow the user to see himself on the newsfeed
+        
+        @activities = PublicActivity::Activity.where(owner_id: friends_ids, owner_type: "User")
         end
     end
 
